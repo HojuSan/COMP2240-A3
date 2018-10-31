b1257941654f0be8712e2d42b67f9de88eb36766
HojuSan <juyong.kim@uon.edu.au>
2018-11-01 10:43:33 +1100

    finished commenting Page

--- Page.java
+++ Page.java
@@ -3,38 +3,43 @@
 //  * Assignment:               Assignment3
 //   * Name:                    Juyong Kim  
 //    * Student Number:         c3244203
-//     * Purpose:               Primary file and then blah blah more explanation of shiet
+//     * Purpose:               Saves the data for pages within a process
 //      * 
 public class Page
 {
+    //variables
     private String id;
     private int processingTime;
     private final int memPro = 6;
 
+    //constructor
     public Page(String id)
     {
         this.id = id;
         processingTime = 0;
     }
 
-    public String getPageId()
-    {
-        return id;
-    }
-    public void upPt()
-    {
+    //functionsS
+    public void upPt()                      //ups prosessing time to 6
+    {                                       //used for memory allocation
         processingTime++;
     }
-    public int getPt()
-    {
-        return processingTime;
-    }
-    public boolean checkPt()
-    {
-        if(memPro == processingTime)
+    public boolean checkPt()                //boolean check of processing time to show
+    {                                       //it has fully loaded into memory
+        if(memPro == processingTime)        
         {
             return true;
         }
         return false;
+    }
+
+    //getters
+    public String getPageId()
+    {
+        return id;
+    }
+    public int getPt()
+    {
+        return processingTime;
     }
 }
